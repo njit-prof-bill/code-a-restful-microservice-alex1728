782751f2c96879e4b8db1d0f6f8a38f418130511
Alex Ack <ackalex16@gmail.com>
2024-10-15 12:34:29 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -2,17 +2,66 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 // Middleware to parse JSON bodies
 app.use(express.json());
+
+
 
 // **************************************************************
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
 
+let users = [];
+let currentId = 1;
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.post('/users', (req, res) => {
+    const { name, email } = req.body;
+    if (!name || !email) {
+        return res.status(400).json({ error: "Name and email are required" });}
+     const newUser = {
+        id: currentId++,
+        name,
+        email
+        };
+    users.push(newUser);
+    res.status(201).json(newUser);
+}); 
+
+app.get('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const user = users.find(u => u.id === userId)
+    if (!user) {
+        return res.status(404).json({ error: "User not found" });
+    }
+    res.status(200).json(user);
+});
+
+app.put('/users/:id', (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id))
+    if (!user) {
+        return res.status(404).json({ error: "User not found" });
+    }
+    const { name, email } = req.body; 
+    if (name) user.name = name;
+    if (email) user.email = email;
+    res.status(200).json(user)
+})
+
+
+app.delete('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const userIndex = users.findIndex(u => u.id === userId);
+    if (userIndex === -1) {
+        return res.status(404).json({ error: "User not found" });
+    }
+    const deletedUser = users.splice(userIndex, 1)[0];
+    res.status(204).json({ message: `User with ID ${deletedUser.id} successfully deleted`, deletedUser });
+});
 // Do not touch the code below this comment
 // **************************************************************
 
